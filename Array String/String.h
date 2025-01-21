f7e5feee53aaf7fc283b17d9c9a500fe9728b599
Maks <maks35@gmail.com>
2025-01-21 23:34:00 +0200

    Constructor for String

--- Array String/String.h
+++ Array String/String.h
@@ -1,30 +1,49 @@
-//#ifndef STRING_H
-//#define STRING_H
-//
-//#include <iostream>
-//#include <cstring>
-//
-//class String {
-//private:
-//	char* str;
-//	size_t length;
-//
-//public:
-//	String();
-//
-//	String(size_t size);
-//
-//	String(const char* initStr);
-//
-//	String(const String& other);
-//
-//	~String();
-//
-//	void input() const;
-//	void output() const;
-//
-//	size_t getLength() const;
-//};
-//
-//
-//#endif // STRING_H
+#ifndef STRING_H
+#define STRING_H
+
+#include <iostream>
+#include <cstring>
+
+class String {
+private:
+	char* str;
+	size_t length;
+
+public:
+	String();
+
+	String(size_t size);
+
+	String(const char* initStr);
+
+	String(const String& other);
+
+	~String();
+
+	String& operator=(const String& other);
+
+	String operator+(const String& other) const ;
+
+	String& operator+=(const String& other);
+
+	char operator[](size_t index) const;
+
+	friend std::ostream& operator<<(std::ostream& os, const String& str);
+
+	bool operator==(const String& other) const;
+
+	bool operator!=(const String& other) const;
+
+	bool operator>(const String& other) const;
+
+	bool operator<(const String& other) const;
+
+	void input() ;
+
+	void output() const;
+
+	size_t getLength() const;
+};
+
+
+#endif // STRING_H
